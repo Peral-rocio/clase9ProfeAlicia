54e7af5ac8df371bee01ad59d798e79b113b6717
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 12:48:45 -0300

    ejercicio uno

---
+++ ejercicioUno.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//******************Ejercicio UNO
+
+// Declara dos variables numéricas numero1 y numero2. Pide al usuario que ingrese dos números y
+// muestra cuál es mayor o si son iguales.
+// PSEUDOCÓDIGO
+// 1. Declarar dos variables para almacenar los números.
+// 2. Pedir al usuario que ingrese dos números.
+// 3. Comparar los números y mostrar el resultado.
+
+//SOLUCION
+
+let numeroUno = prompt("INGRESE SU PRIMER NUMERO");
+let numeroDos = prompt ("INGRESE SU SEGUNDO NUMERO");
+
+if (numeroUno > numeroDos){
+    console.log ( numeroUno + " " + "es mayor a" + " " + numeroDos);
+} else if (numeroUno == numeroDos) {
+    console.log ("Son el mismo numero");
+} else if (numeroUno < numeroDos) {
+    console.log (numeroUno + " " + "es menor a " + " " + numeroDos)
+}
