7830d5ba3c65de23b3ab7b26ee556bc09c392565
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 13:27:14 -0300

    Ejercicio Nueve

---
+++ ejercicioNueve.js
@@ -0,0 +1,19 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//****************Ejercicio NUEVE/
+
+// Define una constante PI con el valor de pi (3.14159). Pide al usuario que ingrese el radio de un
+// círculo y calcula su área y perímetro utilizando la constante PI.
+// PSEUDOCÓDIGO
+// 1. Definir una constante PI con el valor de 3.14159.
+// 2. Solicitar al usuario que ingrese el radio del círculo y almacenarlo en una variable.
+// 3. Calcular el área y el perímetro utilizando las fórmulas correspondientes.
+// AYUDA: El área de un círculo se calcula como PI * radio^2 y el perímetro como 2 * PI * radio.
+
+// const valorPi = 3.14159
+
+//  let radio = parseInt (prompt ("ingrese el radio de su Circulo, porfavor"));
+//  let area = valorPi * (radio * radio);
+//  let perimetro = 2 * (valorPi * radio);
+
+// alert (`El area de su circulo es ${area} y el perimetro es ${perimetro}`);
