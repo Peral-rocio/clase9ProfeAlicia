1532195a3a82bf99a384cb5070b512964eaeeb55
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 13:23:16 -0300

    Ejercicio Siete

---
+++ ejercicioSiete.js
@@ -0,0 +1,15 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 7:
+// Pide al usuario que ingrese su peso en kilogramos y conviértelo a libras. Muestra el resultado con
+// un mensaje. (Averigua como pasar de kg a libras, Pista: 2.20462)
+// PSEUDOCÓDIGO
+// 1. Solicitar al usuario que ingrese su peso en kilogramos y almacenarlo en una variable.
+// 2. Convertir el peso a libras multiplicando por 2.20462.
+// 3. Mostrar el resultado con un mensaje.
+
+//SOLUCION 
+
+// let peso = prompt ("Ingresa tu peso en kilogramos porfavor");
+
+// alert (peso * 2.20462); 
