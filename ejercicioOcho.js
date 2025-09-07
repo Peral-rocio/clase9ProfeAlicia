c2b07d1ead1d158871985212a8e3df55e0b51d66
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 13:25:40 -0300

    Ejercicio Ocho

---
+++ ejercicioOcho.js
@@ -0,0 +1,28 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//****************Ejercicio OCHO
+
+// Ejercicio 8:
+// Pide al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina y muestra si
+// el triángulo es equilátero, isósceles o escaleno. (Investiga sobre los triángulos para determinar la
+// formula)
+// PSEUDOCÓDIGO
+// 1. Solicitar al usuario que ingrese las longitudes de los tres lados y almacenarlas en variables.
+// 2. Comparar los lados para determinar el tipo de triángulo.
+// 3. Mostrar el resultado.
+// AYUDA: Un triángulo es equilátero si los tres lados son iguales, isósceles si dos lados son iguales y
+//escaleno si todos los lados son diferentes.
+
+// SOLUCION
+
+// let lado1 = prompt ("ingrese la longuitd de su primer lado");
+// let lado2 = prompt ("ingrese la longuitd de su segundo lado");
+// let lado3 = prompt ("ingrese la longuitd de su tercer lado");
+
+// if (lado1 == lado2 && lado1 == lado3){
+//     alert (" Tu triangulo es un EQUILATERO")
+// } else if ( lado1 == lado2 && lado1 != lado3 ){
+//     alert ("Tu trianfulo es un Isoceles")
+// } else {
+//     alert ("tu tiangulo es un ESCALENO")
+// }
