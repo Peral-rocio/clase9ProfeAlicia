ac997eb5f037c948f63e3c4ddb73577ea14cb030
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 13:20:42 -0300

    Ejercicio Seis

---
+++ ejercicioSeis.js
@@ -0,0 +1,24 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//********* Ejercicio SEIS 
+
+// Ejercicio 6:
+// Pide al usuario que ingrese su edad y verifica si es mayor o menor de edad. Muestra un mensaje
+// personalizado según el caso.
+// PSEUDOCÓDIGO
+// 1. Solicitar al usuario que ingrese su edad y almacenarla en una variable.
+// 2. Verificar si la edad es mayor o menor de edad.
+// 3. Mostrar un mensaje personalizado según el caso.
+
+// SOLUCION 
+
+// let edadUsuario = prompt("Ingresa tu edad")
+
+
+// if (edadUsuario >= 18) {
+//     alert("HOLA, BIENVENIDO, ERES MAYOR A 18 AÑOS");
+// } else if (edadUsuario <= 0) {
+//     alert("NO EXISTE");
+// } else {
+//     alert("NO PUEDES PASAR, ERES MENOR DE EDAD.");
+// }
