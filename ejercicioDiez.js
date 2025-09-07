acf92894cf28a3261f516ab7f27353e3e09b16d8
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-07 13:29:15 -0300

    Ejercicio Diez

---
+++ ejercicioDiez.js
@@ -0,0 +1,44 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//**********************Ejercicio 10:
+// Pide al usuario que ingrese un número del 1 al 7 y muestra el día de la semana correspondiente. Si
+// el número no está dentro de ese rango, muestra un mensaje de error.
+// PSEUDOCÓDIGO
+// 1. Solicitar al usuario que ingrese un número del 1 al 7 y almacenarlo en una variable.
+// 2. Verificar el valor del número y mostrar el día de la semana correspondiente.
+// 3. Si el número no es válido, mostrar un mensaje de error.
+// AYUDA: Los días de la semana son: 1. Lunes, 2. Martes, 3. Miércoles, 4. Jueves, 5. Viernes, 6.
+// Sábado, 7. Domingo
+// Puedes usar if anidados para verificar el día, o switch para simplificar código.
+
+
+// SOLUCION 
+
+// let numeroUsurario = prompt ("ingrese un numero del 1 al 7, porfavor");
+
+// switch (numeroUsurario){
+//     case "1":
+//         document.write ("Lunes");
+//     break;
+//      case "2":
+//         document.write ("Martes");
+//     break;
+//      case "3":
+//         document.write ("Miercoles");
+//     break;
+//      case "4":
+//         document.write ("Jueves");
+//     break;
+//      case "5":
+//         document.write ("Viernes");
+//     break;
+//      case "6":
+//         document.write ("Sabado");
+//     break;
+//      case "7":
+//         document.write ("Domingo");
+//     break;
+//     default:
+//       alert ("Error. Porfavor ingrese un numero del 1 al 7")
+//     break
+// }
